c104735797f413c4d685154f93463838654009e0
josem <josem@LAPTOP-6NEF8T4E>
2020-01-10 10:27:47 +0100

    Bola en pantalla

--- src/main/java/rg/quintana/pong/App.java
+++ src/main/java/rg/quintana/pong/App.java
@@ -2,8 +2,8 @@ package rg.quintana.pong;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 /**
@@ -13,13 +13,17 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        
+        StackPane root = new StackPane();
+        var scene = new Scene(root, 640, 480);
         stage.setScene(scene);
         stage.show();
+        
+        Circle circleBall = new Circle();
+        circleBall.setCenterX(10);
+        circleBall.setCenterY(30);
+        circleBall.setRadius(7);
+        root.getChildren().add(circleBall);
     }
 
     public static void main(String[] args) {
